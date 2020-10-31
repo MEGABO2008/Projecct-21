e474f2355bc2980cb1866628eefcf95c68e5ff5c
Bryden Ong <71949858+MEGABO2008@users.noreply.github.com>
2020-10-31 18:06:59 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+var bullet, wall, thickness;
+var speed, weight;
+  
+  
+  function setup() {
+  createCanvas(1600,400);
+  speed=random(30,52);
+  weight=random(30,52);
+  thickness=random(22,83);
+  bullet=createSprite(50, 200, 50, 10);
+  bullet.velocityX=speed;
+  bullet.shapeColor = "white";
+  wall=createSprite(1200,200,thickness,height/2);
+  wall.shapeColor = "gray";
+
+}
+function draw() {
+  background(0);  
+  if(hasCollided(bullet, wall)){
+ var damage=0.5 * speed* speed/(thickness *thickness * thickness)
+if(damage>10){
+  wall.shapeColor=color(255,0,0);
+} 
+if(damage<10){
+  wall.shapeColor=color(0,255,0);
+}
+}
+    drawSprites();
+  }
+//┏┛▌▀▐┗┓
+
+function hasCollided(lbullet,lwall) {
+  bulletRightEdge=lbullet.x + lbullet.width;
+  wallLeftEdge=lwall,x;
+  if (bulletRightEdge>=wallLeftEdge){
+return true;
+  }
+return false;
+  }
+
+
+
